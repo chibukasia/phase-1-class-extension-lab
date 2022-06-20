771f6c64da423d596b194b27b4657e9ec15251cf
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-06-21 02:02:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+class Polygon {
+    constructor(array){
+        this.array =array;
+    }
+
+    get countSides (){
+        return this.array.length;
+    }
+
+    get perimeter(){
+        this.array.reduce((accum, curr)=>{
+            accum +=curr;
+        }, 0)
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if ((this.array[1] + this.array[2]) > this.array[3]||
+            (this.array[2] + this.array[3]) > this.array[1]||
+            (this.array[3] + this.array[1]) > this.array[2]
+
+        ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
